238bb23e8cd6ea5894a6c676d19e29b185846cac
sai-cloud-devops <105658722+sai-cloud-devops@users.noreply.github.com>
2024-01-07 09:52:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "dreamtech_technologies_test" {
   bucket = "dreamtech-technologies-test"
 
   # Other bucket configuration settings like tags, encryption, etc.
+}
+
+resource "aws_s3_bucket_public_access_block" "dreamtech_technologies_test_access_block" {
+  bucket = aws_s3_bucket.dreamtech_technologies_test.id
 
   block_public_acls   = true
   ignore_public_acls  = true

4b69e61be9ad2eaad540d6226a55aef06d314350
sai-cloud-devops <105658722+sai-cloud-devops@users.noreply.github.com>
2024-01-07 09:57:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@
 provider "aws" {
 
 	region = var.aws_region   #region where you aws resources will be deployed
-	access_key = var.aws_access_key
-	secret_key = var.aws_secret_key
-
+	assume_role {
+   	 role_arn = "arn:aws:iam::481085503424:role/Ec2RoleDemo"
+ 	}
 }

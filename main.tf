3485cb4a9e9b79236b509af09500dde77d58d991
sai-cloud-devops <105658722+sai-cloud-devops@users.noreply.github.com>
2024-01-07 09:50:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
 resource "aws_s3_bucket" "dreamtech_technologies_test" {
   bucket = "dreamtech-technologies-test"
 
-  tags = {
-    Name        = "DreamtechTechnologiesTestBucket"
-    Environment = "Production"
-  }
-}
+  # Other bucket configuration settings like tags, encryption, etc.
 
-resource "aws_s3_bucket_acl" "dreamtech_technologies_test_acl" {
-  bucket = aws_s3_bucket.dreamtech_technologies_test.id
-
-  # Specify the ACL here, for example:
-  grant {
-    permissions = "private"
-  }
+  block_public_acls   = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
